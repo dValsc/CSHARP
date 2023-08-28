649e8a938be4789d1dbb8afcf4682a4fc2e37fda
dValsc <valeria.dcarvalho@hotmail.com>
2023-08-28 20:46:37 -0300

    adicionado aula5

---
+++ Aula05/EXERCICIO2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EXERCICIO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[,] matriz01 = new float[10, 4];
+            float[] vetorMedia = new float[10];
+
+            for (int linha = 0; linha < matriz01.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matriz01.GetLength(1); coluna++)
+                {
+                    Console.WriteLine($"Digite um valor para a posição [{linha}, {coluna}]: ");
+                    matriz01[linha, coluna] = float.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+            }
+
+            for (int linha = 0; linha < matriz01.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matriz01.GetLength(1); coluna++)
+                {
+                    vetorMedia[linha] += matriz01[linha, coluna]/4 ;
+                }
+
+            }
+
+            for (int linha = 0; linha < matriz01.GetLength(0); linha++)
+            {
+                Console.WriteLine( $"media de todos os elementos da linha ({linha + 1}) é: {vetorMedia[linha]:F1}");
+            }
+        }
+    }
+}
---
+++ Aula05/Exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetorInteiros = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
+            int numero;
+
+            Array.Sort(vetorInteiros);
+
+            Console.WriteLine("Digite o número que deseja encontar: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            int posicao = Array.BinarySearch(vetorInteiros, numero);
+
+            if (posicao>=0)
+                Console.WriteLine($"O número {numero} está localizado na posição: {posicao}");
+            else
+                Console.WriteLine($"O número {numero} não foi encontrado!");
+
+        }
+    }
+}
