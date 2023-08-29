266942ff4ff435039a3e0efd36cfb57da976d823
dValsc <valeria.dcarvalho@hotmail.com>
2023-08-29 17:27:04 -0300

    Adicionado exercicios da aula06

---
+++ Aula06/Exercicio 1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cores= new List<string>();
+            string cor;
+           
+            for (int i = 0; i < 5; i ++)
+            {
+                Console.WriteLine("Digite uma cor: ");
+                cor = Console.ReadLine();
+                cores.Add(cor);
+            }
+         
+
+            foreach (string cor1 in cores)
+            {
+                Console.WriteLine(cor1);
+            }
+
+           cores.Sort();
+
+            foreach (string cor1 in cores)
+            {
+                Console.WriteLine(cor1);
+            }
+
+        }
+    }
+}
